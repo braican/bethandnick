6899a046294391bab4a83dec809591779e022eb8
Nick Braica <nick.braica@gmail.com>
2019-05-02 22:14:20 -0700

    back to the right name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql, Link } from 'gatsby';
 
-import './hstyles.scss';
+import './header.scss';
 
 const HeaderBanner = () => (
   <h1 className="HeaderBanner">
