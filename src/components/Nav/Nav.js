4feeb4ca592ea6eb97e5c7d9f56d780e1711bd1e
Nick Braica <nick.braica@gmail.com>
2019-05-20 17:06:12 -0700

    Fix single landing on page active class

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -84,7 +84,7 @@ class Nav extends React.Component {
                   .map(({ node }) => (
                     <li key={node.id} className="mainnav__item">
                       <Link
-                        to={`/${node.slug}`}
+                        to={`/${node.slug}/`}
                         className="mainnav__link"
                         activeClassName="mainnav__link--active"
                       >
@@ -94,7 +94,7 @@ class Nav extends React.Component {
                   ))}
                 <li className="mainnav__item">
                   <Link
-                    to="/gallery"
+                    to="/gallery/"
                     className="mainnav__link"
                     activeClassName="mainnav__link--active"
                   >
