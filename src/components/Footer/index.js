3d625ef7ce6b7d8d4dde87cbef13fb67770b83e0
Nick Braica <nick.braica@gmail.com>
2019-12-27 15:08:24 -0800

    Adding menu to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Menu from '../Menu';
+
 import styles from './Footer.module.scss';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <p>
-      &copy; {new Date().getFullYear()} Beth and <a target="_blank" rel="noopener noreferrer" href="https://www.braican.com">Nick</a>
+    <p className={styles.copyright}>
+      &copy; {new Date().getFullYear()} Beth and{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://www.braican.com">
+        Nick
+      </a>
     </p>
+
+    <Menu liClass={styles.navListItem} activeClass={styles.activeNavLink} />
   </footer>
 );
 
---
+++ src/components/Menu/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql, Link } from 'gatsby';
+
+const Menu = ({ allWordpressPage, ulClass, liClass, linkClass, activeClass, onClick }) => (
+  <ul className={ulClass}>
+    <li className={liClass}>
+      <Link to="/" className={linkClass} activeClassName={activeClass} onClick={onClick}>
+        Home
+      </Link>
+    </li>
+    {allWordpressPage.edges
+      .filter(({ node }) => node.slug !== 'home')
+      .map(({ node }) => (
+        <li key={node.id} className={liClass}>
+          <Link
+            to={`/${node.slug}/`}
+            className={linkClass}
+            activeClassName={activeClass}
+            onClick={onClick}
+          >
+            {node.acf.menu_label || node.title}
+          </Link>
+        </li>
+      ))}
+    <li className={liClass}>
+      <Link to="/gallery" className={linkClass} activeClassName={activeClass} onClick={onClick}>
+        Photos
+      </Link>
+    </li>
+  </ul>
+);
+
+Menu.propTypes = {
+  allWordpressPage: PropTypes.shape({
+    edges: PropTypes.array,
+  }).isRequired,
+  ulClass: PropTypes.string,
+  liClass: PropTypes.string,
+  linkClass: PropTypes.string,
+  activeClass: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export const pagesQuery = graphql`
+  query NavPagesQuery {
+    allWordpressPage(sort: { fields: [menu_order], order: ASC }) {
+      edges {
+        node {
+          id
+          title
+          slug
+          acf {
+            menu_label
+          }
+        }
+      }
+    }
+  }
+`;
+
+const MenuWithQuery = props => (
+  <StaticQuery query={pagesQuery} render={data => <Menu {...props} {...data} />} />
+);
+
+export default MenuWithQuery;
