dd9c48c1c8a7e9f39c35a8b2826ebcddad5e981f
Nick Braica <nick.braica@gmail.com>
2019-12-27 17:22:13 -0800

    Clean up the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,32 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import Menu from '../Menu';
 
 import styles from './Footer.module.scss';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <p className={styles.copyright}>
-      &copy; {new Date().getFullYear()} Beth and{' '}
+    <div className={styles.main}>
+      <div>
+        <p>Beth &amp; Nick | October 17, 2020 at <Link to="/the-venue/">The Barn at Gibbet Hill</Link>.</p>
+
+        <p className={styles.copyright}>
+      &copy; {new Date().getFullYear()} Beth &amp;{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://www.braican.com">
+        Nick
+          </a>
+        </p>
+      </div>
+
+      <Menu liClass={styles.navListItem} activeClass={styles.activeNavLink} />
+    </div>
+
+    <p className={styles.copyrightMobile}>
+      &copy; {new Date().getFullYear()} Beth &amp;{' '}
       <a target="_blank" rel="noopener noreferrer" href="https://www.braican.com">
         Nick
       </a>
     </p>
-
-    <Menu liClass={styles.navListItem} activeClass={styles.activeNavLink} />
   </footer>
 );
 
