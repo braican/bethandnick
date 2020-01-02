055c4fbd9e9dbbfc9010593b110b250b069683b1
Nick Braica <nick.braica@gmail.com>
2020-01-02 12:54:47 -0800

    Add registry links

---
+++ src/templates/page-registry.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { contentFilter } from '../util';
+
+import SplitLayout from '../layouts/Split';
+import Seo from '../components/Seo';
+import Registry from '../components/Registry';
+
+const Page = ({ data, pageContext: { slug } }) => {
+  const { content, title, acf: { page_featured_image } } = data.wordpressPage;
+  const featuredImage = page_featured_image ? page_featured_image.localFile.childImageSharp.fluid : null;
+  const filteredContent = contentFilter(content);
+
+  return (
+    <SplitLayout featuredImage={featuredImage} bigHeader={slug === 'home'}>
+      <Seo title={title} />
+      <div className="content__main" dangerouslySetInnerHTML={{ __html: filteredContent }} />
+      <Registry />
+    </SplitLayout>
+  );
+};
+
+Page.propTypes = {
+  data: PropTypes.shape({
+    wordpressPage: PropTypes.shape({
+      content: PropTypes.string,
+      title: PropTypes.string,
+      acf: PropTypes.shape({
+        page_featured_image: PropTypes.object,
+      }),
+    }),
+  }),
+  pageContext: PropTypes.shape({
+    slug: PropTypes.string,
+  }),
+};
+
+export default Page;
+
+export const query = graphql`
+  query($slug: String!) {
+    wordpressPage(slug: { eq: $slug }) {
+      title
+      content
+      acf {
+        page_featured_image {
+          localFile {
+            childImageSharp {
+              fluid(maxWidth: 680, quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
