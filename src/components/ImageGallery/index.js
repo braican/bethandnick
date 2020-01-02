7fe4fc6cd54fc0546ffd65a709cc51ef73a5b6c0
Nick Braica <nick.braica@gmail.com>
2020-01-02 14:25:32 -0800

    Alternate photo layout

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
+import { TransitionGroup, Transition } from 'react-transition-group';
+import VisibilitySensor from 'react-visibility-sensor';
 
-import GalleryImage from './Image';
+const timeout = 100;
+
+const getTransitionStyles = {
+  entering: {
+    overflow: 'hidden',
+    width: 0,
+    height: 0,
+    opacity: 0,
+    transform: 'scale(.98)',
+  },
+  entered: {
+    transition: `opacity ${timeout * 2}ms ease-in-out, transform ${timeout * 6}ms ease-out`,
+    transform: 'scale(1)',
+    opacity: 1,
+  },
+  exiting: {
+    transition: `opacity ${timeout}ms ease-in-out`,
+    opacity: 0,
+  },
+};
+
 
 import styles from './ImageGallery.module.scss';
 
 const ImageGallery = ({ wordpressBethandnickGallery: images }) => {
   const { gallery } = images;
 
-  if (!gallery) {
+
+  if (!gallery || gallery.length < 1) {
     return null;
   }
 
-  // If we're on a desktop, enhance the gallery to the horizontal scroller.
+  const [img, setImg] = useState(gallery[0].image.localFile.childImageSharp.fluid.src);
+
   return (
     <div className={styles.gallery}>
+      <div className={styles.galleryImage}>
+        <TransitionGroup>
+          <Transition key={img} timeout={timeout}>{status => <div style={{ ...getTransitionStyles[status] }}><img className={styles.img} src={img} alt=""/></div>}</Transition>
+        </TransitionGroup>
+      </div>
+
       {gallery.map(({ image, caption }) => (
-        <GalleryImage image={image} caption={caption} key={image.id} />
+
+        <div key={image.id} className={styles.imageTrigger}>
+          <VisibilitySensor
+            onChange={visible => {
+              if (visible) {
+                setImg(image.localFile.childImageSharp.fluid.src);
+              }
+            }}
+          >
+            <p className={styles.caption}>{caption}</p>
+          </VisibilitySensor>
+        </div>
       ))}
+
+      {/* {gallery.map(({ image, caption }) => (
+        <GalleryImage image={image} caption={caption} key={image.id} />
+      ))} */}
     </div>
   );
 };
@@ -46,7 +91,7 @@ const ImageGalleryWithQuery = () => (
               localFile {
                 childImageSharp {
                   fluid(maxWidth: 1620, quality: 90) {
-                    ...GatsbyImageSharpFluid
+                    src
                     presentationWidth
                   }
                 }
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,19 +3,25 @@ import React from 'react';
 import Seo from '../components/Seo';
 import SimpleLayout from '../layouts/Simple';
 import ImageGallery from '../components/ImageGallery';
+import Footer from '../components/Footer';
 
 const GalleryPage = () => (
   <SimpleLayout>
-    <Seo title="Gallery" />
+    <Seo title="Photos" />
 
-    <div className="simplelayout__main">
-      <h2 className="page-title">Photos</h2>
+    <div className="splitpane__content">
       <div className="content__main">
-        <p>We've got some good ones over the years.</p>
+        <h2>Photos</h2>
+        <div className="content__main">
+          <p>We've got some good ones. Scroll down to flip through some photos of Beth &amp; Nick through the years.</p>
+        </div>
       </div>
+
+      <ImageGallery />
+
+      <Footer />
     </div>
 
-    <ImageGallery />
   </SimpleLayout>
 );
 
