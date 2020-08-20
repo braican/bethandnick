4de345fc8d057343697c9f8726e643959f725301
Nick Braica <nick.braica@gmail.com>
2020-08-19 22:02:49 -0700

    Small style update

--- src/components/CovidAlert/index.js
+++ src/components/CovidAlert/index.js
@@ -5,8 +5,8 @@ import styles from './CovidAlert.module.scss';
 
 const CovidAlert = () => (
   <div className={styles.alert}>
-    Due to the ongoing situation with Covid-19, we've opted to postpone our wedding until December
-    18th of 2021.
+    Due to the ongoing situation with Covid-19, we've decided to postpone our wedding until December
+    18, 2021.
   </div>
 );
 
